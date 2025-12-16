7343cffdb397dfa2643a90366fb0852c23ceb11b
Aakshat-01 <aakshatgupta40@gmail.com>
2025-12-16 13:06:36 +0530

    Create 7.py

---
+++ Day 1/7.py
@@ -0,0 +1,14 @@
+#Problem 13
+temp=24
+if(temp < 0):
+    print("Freezing weather")
+elif(temp>0 and temp<10):
+    print("Very Cold weather")
+elif(temp>10 and temp<20):
+    print("Cold weather")
+elif(temp>20 and temp<30):
+    print("Normal in Temp")
+elif(temp>30 and temp<40):
+    print("Its Hot")
+elif(temp >=40):
+    print("Its Very Hot")
