39eb8e8cb19d722a2fdad60facf6899d7a0ddf7a
Aakshat-01 <aakshatgupta40@gmail.com>
2025-12-16 13:05:51 +0530

    Practising

---
+++ Day 1/3.py
@@ -0,0 +1,7 @@
+#Problem 3 
+a=13
+if(a>0):
+    print("Positive")
+else:
+    print("Negative")
+
---
+++ Day 1/4.py
@@ -0,0 +1,7 @@
+#Problem 5
+age=13
+if(age>18):
+    print("Congratulation! You are eligible for casting your vote.")
+else:
+    print("You are not eligible for casting your vote.")
+
