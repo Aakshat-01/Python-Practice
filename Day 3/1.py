b4402db85b1a05d163c260312c9ba0b3e869c4b3
Aakshat-01 <aakshatgupta40@gmail.com>
2025-12-18 12:06:30 +0530

    Create 1.py

---
+++ Day 3/1.py
@@ -0,0 +1,14 @@
+#Problem 1
+for i in range(20,0,-3):
+    print(i)
+
+#Problem 2
+s=0
+for i in range(1,11):
+    s+=i
+print(s)
+
+#Problem 3
+n=int(input("Input number of terms:"))
+for i in range(1,n+1):
+    print("Number is",i,"and Cube is",i**3)
