d2f8c0c90c1ac0f8e9a2f82627a5b03195094856
Aakshat-01 <aakshatgupta40@gmail.com>
2025-12-19 12:23:32 +0530

    Update 1.py

--- Day 4/1.py
+++ Day 4/1.py
@@ -1,8 +1,10 @@
+#Problem 1
 for i in range(1,5):
     for j in range(1,i+1):
         print("*", end=" ")
     print()
 
+#Problem 2
 for i in range(1,5):
     for k in range(4-i,0,-1):
         print(" ", end=" ")
@@ -10,6 +12,7 @@ for i in range(1,5):
         print("*", end=" ")
     print()
 
+#Problem 3
 for i in range(1,6):
     for j in range(1,i+1):
         print(j, end=" ")
