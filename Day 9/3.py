76d95f32ed3656d156f031ceac72b8bbdc4dc436
Aakshat-01 <aakshatgupta40@gmail.com>
2025-12-24 12:12:08 +0530

    Create 3.py

---
+++ Day 9/3.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+    def display(self):
+        t=self
+        while t:
+            print(t.data, end="->")
+            t=t.next
+n1=Node(10)
+n2=Node(20)
+n3=Node(30)
+n4=Node(40)
+n1.next=n2
+n2.next=n3
+n3.next=n4
+n1.display()
