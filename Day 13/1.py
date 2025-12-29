189f2a778ed5f75c6657367d6eeb51bb9a9a9b91
Aakshat-01 <aakshatgupta40@gmail.com>
2025-12-29 13:40:58 +0530

    Create 1.py

---
+++ Day 13/1.py
@@ -0,0 +1,15 @@
+#Leet Code Problem 876
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        s=head
+        f=head
+        while f and f.next:
+            s=s.next
+            f=f.next.next
+        return s
