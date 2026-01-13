3fc161fb33bef4529d831b3ba530b44d0c079ce0
Aakshat-01 <aakshatgupta40@gmail.com>
2026-01-13 12:06:04 +0530

    Update 4.py

--- HashTable/4.py
+++ HashTable/4.py
@@ -4,23 +4,14 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        # if len(set(s)) == len(set(t)):
-        #     return True
-        # else:
-        #     return False
         d_s_t={}
         d_t_s={}
         li=[]
         for i in range(len(s)):
             d_s_t[s[i]]=t[i]
             d_t_s[t[i]]=s[i]
-        print(d_s_t)
-        print(d_t_s)
-        # for i in range(len(s)):
-        #     li.append(d_s_t[s[i]])
-        # print(li)
-        # if "".join(li) == t:
-        #     return True
-        # else:
-        #     return False
         
+        if list(d_s_t.keys()) == list(d_t_s.values()):
+            return True
+        else:
+            return False
