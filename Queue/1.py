a0f2a4209bf6229d16a3e6ac4bfb9f89e8de2f7c
Aakshat-01 <aakshatgupta40@gmail.com>
2026-01-14 12:40:18 +0530

    Create 1.py

---
+++ Queue/1.py
@@ -0,0 +1,40 @@
+#LeetCode Problem 622  Design Circular Queue
+
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.q=[]
+        self.size=k
+
+    def enQueue(self, value: int) -> bool:
+        if len(self.q)!=self.size:
+            self.q.append(value)
+            return True
+        return False
+
+    def deQueue(self) -> bool:
+        if self.q:
+            self.q.pop(0)
+            return True
+        return False        
+
+    def Front(self) -> int:
+        return -1 if not self.q else self.q[0]
+
+    def Rear(self) -> int:
+        return -1 if not self.q else self.q[-1]
+
+    def isEmpty(self) -> bool:
+        return not self.q
+
+    def isFull(self) -> bool:
+        return len(self.q)==self.size
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
