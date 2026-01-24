a64ce8f06ee1222974e721f6f7fedf281c49465d
Aakshat-01 <aakshatgupta40@gmail.com>
2026-01-24 09:54:22 +0530

    Create 9.py

---
+++ Tree/9.py
@@ -0,0 +1,29 @@
+#LeetCode Problem 1161  Maximum Level Sum of a Binary Tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        cl=[root]
+        res=[[root.val]]
+        maxi=float('-inf')
+        while cl:
+            nl=[]
+            for n in cl:
+                if n and n.left:
+                    nl.append(n.left)
+                if n and n.right:
+                    nl.append(n.right)
+            cl=nl
+            if cl:
+                res.append([n.val for n in cl])
+        max_sum=float('-inf')
+        for i in range(len(res)):
+            if sum(res[i])>max_sum:
+                maxi=i+1
+                max_sum=sum(res[i])
+        return maxi
