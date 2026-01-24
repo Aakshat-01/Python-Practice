8ce024b5128251985aed1e73b35cb76db7223fe0
Aakshat-01 <aakshatgupta40@gmail.com>
2026-01-24 09:54:25 +0530

    Create 14.py

---
+++ Tree/14.py
@@ -0,0 +1,29 @@
+#LeetCode Problem 103  Binary Tree Zigzag Level Order Traversal
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        cl=[root]
+        res=[[root.val]]
+        while cl:
+            nl=[]
+            for n in cl:
+                if n and n.left:
+                    nl.append(n.left)
+                if n and n.right:
+                    nl.append(n.right)
+            cl=nl
+            if cl:
+                res.append([n.val for n in cl])
+        for i in range(len(res)):
+            if i%2!=0:
+                res[i]=res[i][::-1]
+        return res
