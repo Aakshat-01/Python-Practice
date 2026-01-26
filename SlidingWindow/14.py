ce09f03072afd8c4788361ef25b3764ae85f0041
Aakshat-01 <aakshatgupta40@gmail.com>
2026-01-26 11:09:38 +0530

    Create 14.py

---
+++ SlidingWindow/14.py
@@ -0,0 +1,10 @@
+#LeetCode Problem 1876 Substrings of Size Three with Distinct Characters
+
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        count = 0
+
+        for i in range(len(s) - 2):
+            if len(set(s[i:i+3])) == 3:
+                count += 1
+        return count
