c31f1f843fb640eecf1e04fdd477fddb97dc798c
Aakshat-01 <aakshatgupta40@gmail.com>
2026-01-28 13:43:20 +0530

    Create 1.py

---
+++ BFS/1.py
@@ -0,0 +1,30 @@
+g={
+    "a":["b","c","d"],
+    "b":["a","c"],
+    "c":["a","b"],
+    "d":["a","e","f"],
+    "e":["d"],
+    "f":["d"]
+}
+
+# #BFS
+# q,v,r=['d'],{'d'},['d']
+# while q:
+#     p=q.pop(0)
+#     for n in g[p]:
+#         if n not in v:
+#             q.append(n)
+#             v.add(n)
+#             r.append(n)
+# print(r)
+
+#DFS
+def dfs(g,n,v,r):
+    if n not in v:
+        v.add(n)
+        r.append(n)
+        for i in g[n]:
+            dfs(g,i,v,r)
+    return r
+
+print(dfs(g,"d",set(),[]))
